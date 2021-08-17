fd4e6858622486da1114d5a5d711a20c311860b3
keishihashimoto <keishi04hrikzira@yahoo.co.jp>
2021-08-17 17:20:20 +0900

    サインインができるようにファイルを編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "is Invalid. Alphabet and number must be used together."}
+  validates :password,
+            format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i,
+                      message: 'is Invalid. Alphabet and number must be used together.' }
   validates :nickname, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is Invalid. Input full-width characters."}
-  validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is Invalid. Input full-width characters."}
+  validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is Invalid. Input full-width characters.' }
+  validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is Invalid. Input full-width characters.' }
   validates :first_name_reading, presence: true
   validates :last_name_reading, presence: true
-  validates :first_name_reading, format: { with: /\A[ァ-ヶ一]+\z/, message: "is Invalid. Input full-width katakana caracters"}
-  validates :last_name_reading, format: { with: /\A[ァ-ヶ一]+\z/, message: "is Invalid. Input full-width katakana caracters"}
+  validates :first_name_reading, format: { with: /\A[ァ-ヶ一]+\z/, message: 'is Invalid. Input full-width katakana caracters' }
+  validates :last_name_reading, format: { with: /\A[ァ-ヶ一]+\z/, message: 'is Invalid. Input full-width katakana caracters' }
   validates :birth_day, presence: true
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,12 +6,12 @@ FactoryBot.define do
     nickname { Faker::Internet.username }
     email { Faker::Internet.free_email }
     password = Faker::Internet.password(min_length: 6)
-    password {password}
-    password_confirmation {password}
+    password { password }
+    password_confirmation { password }
     first_name { person.first.kanji }
     last_name { person.last.kanji }
     first_name_reading { person.first.katakana }
     last_name_reading { person.last.katakana }
-    birth_day { Faker::Date.between(from: "1930-01-01", to: "2016-12-31") }
+    birth_day { Faker::Date.between(from: '1930-01-01', to: '2016-12-31') }
   end
 end
