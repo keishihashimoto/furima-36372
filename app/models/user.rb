d4eb6259c3c2299036e32a6b2bc814be25111a7e
keishihashimoto <keishi04hrikzira@yahoo.co.jp>
2021-08-17 11:11:06 +0900

    不要なバリデーションを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :email, uniqueness: true
   validates :encrypted_password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "is Invalid. Alphabet and number must be used together."}
   validates :first_name, presence: true
   validates :last_name, presence: true
