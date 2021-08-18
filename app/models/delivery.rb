897a7137e93582cd5241620029b4284ec7ab30bf
keishihashimoto <keishi04hrikzira@yahoo.co.jp>
2021-08-18 10:36:03 +0900

    送料、発送日、商品の状態、商品カテゴリーのActiveHashの実装

---
+++ app/models/delivery.rb
@@ -0,0 +1,8 @@
+class Delivery < ActiveHash::Base
+  self.data = [
+    { id: 1, text: "---" },
+    { id: 2, text: "1~2日で発送" },
+    { id: 3, text: "2~3日で発送" },
+    { id: 2, text: "4~7日で発送" }
+  ]
+end
