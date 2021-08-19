b628b3e8916d056ea2f07be251c5f955633869c6
keishihashimoto <keishi04hrikzira@yahoo.co.jp>
2021-08-19 11:47:11 +0900

    不要なコードの削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_item, only: [:show, :edit]
   def index
     @items = Item.all.order(created_at: 'DESC')
   end
@@ -18,12 +19,10 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
-    redirect_to root_path if user_signed_in? && current_user.id != @item.user.id
+    redirect_to root_path if current_user.id != @item.user.id
   end
 
   def update
@@ -40,5 +39,9 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :postage_id, :prefecture_id,
                                  :delivery_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
