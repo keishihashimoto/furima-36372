e8ae32e65bebd0606a14250b5617df627a41f070
keishihashimoto <keishi04hrikzira@yahoo.co.jp>
2021-08-24 17:17:28 +0900

    自身が出品した商品に対してはお気に入り登録をできないように設定

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,12 +1,19 @@
 class FavoritesController < ApplicationController
+  before_action :authenticate_user!, only: [:create], if: :favorites_controller?
   def create
     user_id = current_user.id
-    item_id = params[:item_id]
+    item = Item.find(params[:item_id])
+    item_id = item.id
+    item_user_id = item.user.id
     @Favorite = Favorite.create(favorite_params)
   end
 
   private
   def favorite_params
     params.permit(:item_id).merge(user_id: current_user.id)
+  end
+
+  def favorites_controller?
+    controller_name == "favorites"
   end
 end
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,10 +1,14 @@
 class Favorite < ApplicationRecord
   validates :user_id, numericality: { less_than: 0 }, if: :not_unique_favorite?
+  validates :user_id, numericality: { less_than: 0 }, if: :same_user?
   belongs_to :item
   belongs_to :user
 
   private
   def not_unique_favorite?
     Favorite.exists?(user_id: user_id, item_id: item_id)
+  end
+  def same_user?
+    user_id == item.user.id
   end
 end
